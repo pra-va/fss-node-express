e9d77a789efad9f15e3e017658ed822691f06abe
Pranas Varaneckas <varaneckas@gmail.com>
2021-10-10 21:09:54 +0300

    returning file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,13 @@ var app = express();
 
 console.log("Hello World");
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// });
+
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    const path = __dirname + "/views/index.html";
+    res.sendFile(path);
 });
 
 
